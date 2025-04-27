5cf2f16323f89d01d13fbd100301510a4c8178d3
Srida42 <sridachallapalli@gmail.com>
2025-04-27 19:42:16 +0530

    Sorting  (2 / 5)

---
+++ Sorting/1_sort_descending.py
@@ -0,0 +1,32 @@
+"""Challenge
+Implement the following functions:
+
+sort_words(words: List[str]) -> List[str] - This function accepts a list of words and returns the list of words sorted in descending order.
+
+sort_numbers(numbers: List[int]) -> List[int] - This function accepts a list of numbers and returns the list of numbers sorted in descending order.
+
+sort_decimals(numbers: List[float]) -> List[float] - This function accepts a list of decimal numbers and returns the list of decimal numbers sorted in descending order. """
+
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    words.sort(reverse=True)
+    return words
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    numbers.sort(reverse=True)
+    return numbers
+
+def sort_decimals(numbers: List[float]) -> List[float]:
+    numbers.sort(reverse=True)
+    return numbers
+
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, 5, 3, 2, 4, 11, 19, 9, 2, 5, 6, 7, 4, 2, 6]))
+
+print(sort_decimals([3.14, 2.82, 6.433, 7.9, 21.555, 21.554]))
